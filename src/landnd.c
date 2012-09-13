fc1a466715361b549e2bb5f44dc0da473bf7f20e
Paolo Stivanin <admin@polslinux.it>
2012-09-13 15:08:22 +0200

    CHANGELOG: * added S_ISDIR control; * error checking improvement;

--- src/landnd.c
+++ src/landnd.c
@@ -1,13 +1,18 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
 int main(int argc, char **argv){
 	if(argc != 2){
 		printf("Usage: %s <file-to-send>\n", argv[0]);
 		return EXIT_FAILURE;
 	}
-	char *input_file = NULL;
+	struct stat fileStat;
+	char *input_file = NULL, *token = NULL, *file = NULL;
 	size_t len_input;
 	len_input = strlen(argv[1])+1;
 	input_file = (char *)malloc(len_input);
@@ -16,7 +21,21 @@ int main(int argc, char **argv){
 		return EXIT_FAILURE;
 	}
 	strcpy(input_file, argv[1]);
+	if(stat(input_file, &fileStat) < 0){
+		printf("stat ERROR, exiting...");
+		free(input_file);
+		return EXIT_FAILURE;
+	}
+	if(S_ISDIR(fileStat.st_mode)){
+		printf("ERROR: This is a directory not file, exiting...\n");
+		free(input_file);
+		return EXIT_FAILURE;
+	}
 	printf("%s\n", argv[1]);
+	token = strdup(input_file);
+	file = basename(token); /* basename potrebbe modificare input_file così ne passo una copia e sono al sicuro */
+	printf("%s\n", file);
 	free(input_file);
+	free(token);
 	return 0;
 }
