c6a7bf90421156779f3efb5c97ec3844a34e3707
Paolo Stivanin <polslinux@users.noreply.github.com>
2014-02-03 17:18:58 +0100

    Update do_recv.c
    updated codebase to fix #8

--- src/do_recv.c
+++ src/do_recv.c
@@ -75,163 +75,154 @@ int do_recv(const char *acceptOrNot){
     		close(socketStruct.sockd);
     		return -1;
   	}
-  	if(recv(socketStruct.newsockd, &client_counter, sizeof(client_counter), 0) < 0){
-    		printf("Error on the number of files\n");
-		close(socketStruct.newsockd);
-    		close(socketStruct.sockd);
-    		return -1;
-  	}
-  	while(counter < client_counter){
-    		fsize = total_bytes_read = nread = 0;
-    		memset(yORn, 0, sizeof(yORn));
-    		if(recv(socketStruct.newsockd, &fsize, sizeof(fsize), 0) < 0){
-      			printf("Error on receiving the file name length\n");
-   	  		close(socketStruct.newsockd);
-   	  		close(socketStruct.sockd);
-   	  		return -1;
-    		}
-    		filename = (char *)malloc(fsize);
-    		if(filename == NULL){
-	      		printf("Error during filename memory allocation\n");
-      			close(socketStruct.newsockd);
-      			close(socketStruct.sockd);
-    		}
-    		if(recv(socketStruct.newsockd, filename, fsize, 0) < 0){
-   			printf("Error on receiving the file name\n");
-   	  		close(socketStruct.newsockd);
-   	  		close(socketStruct.sockd);
-   	  		free(filename);
-   	  		return -1;
-    		}
-    		fsize = 0;
-    		if(recv(socketStruct.newsockd, &fsize, sizeof(fsize), 0) < 0){
-	   	  	printf("Error on receiving the file size\n");
-   	  		close(socketStruct.newsockd);
-   	  		close(socketStruct.sockd);
-   	  		free(filename);
-   	  		return -1;
-    		}
-    
-	    	if(strcmp(acceptOrNot, "y") == 0){
-      			strcpy(yORn, "Y");
-      			goto auto_accept;
-    		}
-
-    		printf("Do you want to receive the file '%s' which size is '%"PRIu32"' bytes? (Y or N)\n", filename, fsize);
-    		another_yorn:
-    		if(scanf("%1s", yORn) == EOF){
-	      		printf("Scanf error\n");
-      			close(socketStruct.newsockd);
-      			close(socketStruct.sockd);
-      			free(filename);
-    		}
-    		yORn[0] = toupper(yORn[0]);
-    		if(strcmp(yORn, "Y") != 0){
-	      		if(strcmp(yORn, "N") != 0){
-        			printf("You have to write Y or N, try again: ");
-        			memset(yORn, 0, sizeof(yORn));
-        			goto another_yorn;
-      			}
-    		}
-    		auto_accept:
-    		if(strcmp(yORn, "N") == 0){
-	      		printf("Transfer aborted\n");
-      			if(send(socketStruct.newsockd, yORn, 2, 0) < 0){
-	        		printf("Error on sending N\n");
-        			close(nsocketStruct.ewsockd);
-        			close(socketStruct.sockd);
-        			free(filename);    
-		 	}
-      			close(socketStruct.newsockd);
-      			close(socketStruct.sockd);
-      			free(filename);
-      			return -2;
-    		}
-    		else{
-	      		if(send(socketStruct.newsockd, yORn, 2, 0) < 0){
-        			printf("Error on sending Y\n");
-        			close(socketStruct.newsockd);
-        			close(socketStruct.sockd);
-        			free(filename);    
-      			}
-	    	}
-    		fsize_tmp = fsize;
-    		filebuffer = malloc(fsize);
-    		if(filebuffer == NULL){
-	      		printf("Error during filebuffer memory allocation\n");
-      			close(socketStruct.newsockd);
-      			close(socketStruct.sockd);
-      			free(filename);
-    		}
-		socketStruct.fd = open(filename, O_CREAT | O_WRONLY, 0644);
-    		if(socketStruct.fd  < 0) {
-	   	  	printf("Error during file opening\n");
-   	  		close(socketStruct.newsockd);
-   	  		close(socketStruct.sockd);
-      			free(filename);
-      			free(filebuffer);
-   	  		return -1;
-    		}
-    		tx = 0;
-    		while((total_bytes_read != fsize) && ((nread = read(newsockd, filebuffer, fsize_tmp)) > 0)){
-	      		tx += nread;
-      			printf("\r%zd%%", (tx * 100 / fsize));
-   	  		if(write(fd, filebuffer, nread) != nread){
-	   	    		printf("Write error\n");
-  		  		close(socketStruct.newsockd);
-   	    			close(socketStruct.sockd);
-        			close(socketStruct.fd);
-     	  			free(filename);
-  		  		free(filebuffer);
-     	  			return -1;
-   			}
-   	  		total_bytes_read += nread;
-   	  		fsize_tmp -= nread;
-    		}
-    		char *file_md5 = check_md5(filename);
-    		if(recv(socketStruct.newsockd, hash, 33, 0) < 0){
-	      		printf("Error on receiving file md5\n");
-      			close(socketStruct.newsockd);
-      			close(socketStruct.sockd);
-      			close(socketStruct.fd);
-      			free(filename);
-      			free(file_md5);
-      			return -1;
-    		}
-    		if(strcmp(file_md5, hash) == 0){
-	      		memset(yORn, 0, sizeof(yORn));
-      			strcpy(yORn, "Y");
-      			if(send(socketStruct.newsockd, yORn, 2, 0) < 0){
-	        		close(socketStruct.newsockd);
-        			close(socketStruct.sockd);
-        			close(socketStruct.fd);
-        			free(filename);
-        			free(file_md5);
-        			return -1;
-      			}
-      			printf("\n--> File successfully sent\n");
-    		}
-    		else{
-	      		memset(yORn, 0, sizeof(yORn));
-      			strcpy(yORn, "N");
-      			if(send(socketStruct.newsockd, yORn, 2, 0) < 0){
-	        		close(socketStruct.newsockd);
-        			close(socketStruct.sockd);
-        			close(socketStruct.fd);
-        			free(filename);
-        			free(file_md5);
-        			return -1;
-      			}
-      			printf("\n--> File transfer FAILED, md5sum doesn't match\n");
-    		}
-    		free(file_md5);
-    		close(socketStruct.fd);
-    		free(filename);
-    		free(filebuffer);
-    		//printf("\n");
-    		counter++;
+	
+	fsize = total_bytes_read = nread = 0;
+    	memset(yORn, 0, sizeof(yORn));
+    	if(recv(socketStruct.newsockd, &fsize, sizeof(fsize), 0) < 0){
+      		printf("Error on receiving the file name length\n");
+   		close(socketStruct.newsockd);
+   		close(socketStruct.sockd);
+   		return -1;
     	}
-  	close(socketStruct.newsockd);
+    	filename = (char *)malloc(fsize);
+    	if(filename == NULL){
+		printf("Error during filename memory allocation\n");
+      		close(socketStruct.newsockd);
+      		close(socketStruct.sockd);
+    	}
+    	if(recv(socketStruct.newsockd, filename, fsize, 0) < 0){
+   		printf("Error on receiving the file name\n");
+   		close(socketStruct.newsockd);
+   		close(socketStruct.sockd);
+   		free(filename);
+   		return -1;
+    	}
+    	fsize = 0;
+    	if(recv(socketStruct.newsockd, &fsize, sizeof(fsize), 0) < 0){
+		printf("Error on receiving the file size\n");
+   		close(socketStruct.newsockd);
+   		close(socketStruct.sockd);
+   		free(filename);
+   		return -1;
+    	}
+    
+	if(strcmp(acceptOrNot, "y") == 0){
+      		strcpy(yORn, "Y");
+      		goto auto_accept;
+    	}
+
+    	printf("Do you want to receive the file '%s' which size is '%"PRIu32"' bytes? (Y or N)\n", filename, fsize);
+    	another_yorn:
+    	if(scanf("%1s", yORn) == EOF){
+	      	printf("Scanf error\n");
+      		close(socketStruct.newsockd);
+      		close(socketStruct.sockd);
+      		free(filename);
+    	}
+    	yORn[0] = toupper(yORn[0]);
+    	if(strcmp(yORn, "Y") != 0){
+	      	if(strcmp(yORn, "N") != 0){
+        		printf("You have to write Y or N, try again: ");
+        		memset(yORn, 0, sizeof(yORn));
+        		goto another_yorn;
+        	}
+    	}
+    	auto_accept:
+    	if(strcmp(yORn, "N") == 0){
+	      	printf("Transfer aborted\n");
+      		if(send(socketStruct.newsockd, yORn, 2, 0) < 0){
+	        	printf("Error on sending N\n");
+        		close(nsocketStruct.ewsockd);
+        		close(socketStruct.sockd);
+        		free(filename);    
+		 }
+      		close(socketStruct.newsockd);
+      		close(socketStruct.sockd);
+      		free(filename);
+      		return -2;
+    	}
+    	else{
+		if(send(socketStruct.newsockd, yORn, 2, 0) < 0){
+        		printf("Error on sending Y\n");
+        		close(socketStruct.newsockd);
+        		close(socketStruct.sockd);
+        		free(filename);    
+      		}
+	}
+    	fsize_tmp = fsize;
+    	filebuffer = malloc(fsize);
+    	if(filebuffer == NULL){
+		printf("Error during filebuffer memory allocation\n");
+      		close(socketStruct.newsockd);
+      		close(socketStruct.sockd);
+      		free(filename);
+    	}
+	socketStruct.fd = open(filename, O_CREAT | O_WRONLY, 0644);
+    	if(socketStruct.fd  < 0) {
+		printf("Error during file opening\n");
+		close(socketStruct.newsockd);
+   	  	close(socketStruct.sockd);
+      		free(filename);
+      		free(filebuffer);
+   	  	return -1;
+    	}
+    	tx = 0;
+    	while((total_bytes_read != fsize) && ((nread = read(newsockd, filebuffer, fsize_tmp)) > 0)){
+		tx += nread;
+      		printf("\r%zd%%", (tx * 100 / fsize));
+   	  	if(write(fd, filebuffer, nread) != nread){
+	   	    	printf("Write error\n");
+  		  	close(socketStruct.newsockd);
+   	    		close(socketStruct.sockd);
+        		close(socketStruct.fd);
+     	  		free(filename);
+  		  	free(filebuffer);
+     	  		return -1;
+   		}
+   	  	total_bytes_read += nread;
+   	  	fsize_tmp -= nread;
+    	}
+    	char *file_md5 = check_md5(filename);
+    	if(recv(socketStruct.newsockd, hash, 33, 0) < 0){
+	      	printf("Error on receiving file md5\n");
+      		close(socketStruct.newsockd);
+      		close(socketStruct.sockd);
+      		close(socketStruct.fd);
+      		free(filename);
+      		free(file_md5);
+      		return -1;
+    	}
+    	if(strcmp(file_md5, hash) == 0){
+	      	memset(yORn, 0, sizeof(yORn));
+      		strcpy(yORn, "Y");
+      		if(send(socketStruct.newsockd, yORn, 2, 0) < 0){
+	       		close(socketStruct.newsockd);
+        		close(socketStruct.sockd);
+        		close(socketStruct.fd);
+        		free(filename);
+        		free(file_md5);
+        		return -1;
+      		}
+      		printf("\n--> File successfully received\n");
+    	}
+    	else{
+	  	memset(yORn, 0, sizeof(yORn));
+      		strcpy(yORn, "N");
+      		if(send(socketStruct.newsockd, yORn, 2, 0) < 0){
+	        	close(socketStruct.newsockd);
+        		close(socketStruct.sockd);
+        		close(socketStruct.fd);
+        		free(filename);
+        		free(file_md5);
+        		return -1;
+      		}
+      		printf("\n--> File transfer FAILED, md5sum doesn't match\n");
+    	}
+    	free(file_md5);
+    	close(socketStruct.fd);
+    	free(filename);
+    	free(filebuffer);
+    	close(socketStruct.newsockd);
   	close(socketStruct.sockd);
   	return 0;
 }
